58c8eb73a7830bb628d27f0bea6be9b4994de285
songezo_mtengwana <songezomtengwana@gmail.com>
2023-06-14 11:43:13 +0200

    home screen update

--- app/src/main/java/com/mapitsilicousness/etreprenuercommunity/MainActivity.kt
+++ app/src/main/java/com/mapitsilicousness/etreprenuercommunity/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.mapitsilicousness.etreprenuercommunity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth = Firebase.auth
+
+        val user = auth.currentUser
+
+        val h_displayname = findViewById<TextView>(R.id.home_displayname)
+
+        h_displayname.setText(user?.displayName)
     }
 }
--- app/src/main/java/com/mapitsilicousness/etreprenuercommunity/SignUpActivity.kt
+++ app/src/main/java/com/mapitsilicousness/etreprenuercommunity/SignUpActivity.kt
@@ -1,18 +1,35 @@
 package com.mapitsilicousness.etreprenuercommunity
 
 import android.content.Intent
+import android.net.Uri
+import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.auth.ktx.userProfileChangeRequest
+import com.google.firebase.ktx.Firebase
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
+        auth = Firebase.auth
+
         val login_prompt = findViewById<TextView>(R.id.login_login_prompt)
         val login_login_prompt = findViewById<TextView>(R.id.login_signup)
+
+        val email_value = findViewById<TextInputEditText>(R.id.signup_input_email)
+        val password_value = findViewById<TextInputEditText>(R.id.signup_input_password)
+        val displayName_value = findViewById<TextInputEditText>(R.id.signup_input_name)
+
 
         login_prompt.setOnClickListener {
             var i = Intent(this, SignInActivity::class.java)
@@ -20,8 +37,30 @@ class SignUpActivity : AppCompatActivity() {
         }
 
         login_login_prompt.setOnClickListener{
-            var i = Intent(this , MainActivity::class.java)
-            startActivity(i)
+            val email = email_value.text.toString()
+            val password = password_value.text.toString()
+            val username = displayName_value.text.toString()
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { it ->
+                    if (it.isSuccessful) {
+                        val user = auth.currentUser
+
+                        val profileUpdates = userProfileChangeRequest {
+                            displayName = username
+                        }
+
+                        var i = Intent(this, MainActivity::class.java)
+                        startActivity(i)
+
+                    } else {
+                        Toast.makeText(
+                            baseContext,
+                            "Authentication failed.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                    }
+                }
         }
     }
 }
